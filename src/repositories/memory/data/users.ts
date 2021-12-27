18dc692bf897fcb9e584b37f0d774c13c267a420
Otho Garcia <otho.garcia@rockcontent.com>
2021-12-27 12:43:49 -0300

    Feature: authentication

---
+++ src/repositories/memory/data/users.ts
@@ -0,0 +1,9 @@
+export const users = [
+  {
+    name: 'otho',
+    email: 'othogar@gmail.com',
+    password: '$2b$10$gsiTmFqR/aAdt1SWD2tjgebnMlc24xudVOTCjOA2HFPSViXYVGdUe',
+    confirmPassword: '123',
+    id: 1
+  }
+]
--- src/repositories/memory/user.ts
+++ src/repositories/memory/user.ts
@@ -2,12 +2,13 @@ import { IUser, IUserFilter, IUserInput } from '@/src/domain/entities/interfaces
 import { Injectable } from '@nestjs/common'
 import UserRepository from '../interfaces/user'
 import { maxBy } from 'lodash'
+import { users } from './data/users'
 
 @Injectable()
 export class UserMemoryRepository implements UserRepository {
   private users: IUser[]
   constructor() {
-    this.users = []
+    this.users = users
   }
   async save(user: IUser): Promise<IUser> {
     const lastId = maxBy(this.users, (u) => u.id)?.id || 0
